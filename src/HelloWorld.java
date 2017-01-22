9e7bfb3cfcebed424df010e4c634d6777e2e16bf
Sriram S <s.sriram01@gmail.com>
2017-01-22 17:53:19 +0530

    made changes in the branch1 to test my code.It works

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,12 @@ public class HelloWorld {
 
 public static void public static void main(String[] args) {
 
-	System.out.println("Hello World - my first program for Git and not in java");
+	System.out.println("Hello World - my first program for Git and not in java- Also updated this in the branch");
 	System.out.println("I edited this in the GitHub");
 
+	//Adding in the branch
+
+	System.out.println("Added this comment in the branch");
 }
 
 }
