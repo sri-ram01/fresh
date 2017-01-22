e9ada9d3f740a881b53d78dc6dd905a8e52bc2dc
Sriram S <s.sriram01@gmail.com>
2017-01-22 16:59:03 +0530

    Update the first system print statement in my local repository

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 public static void public static void main(String[] args) {
-	System.out.println("Hello World - my first program");
+	System.out.println("Hello World - my first program for Git and not in java");
 	System.out.println("I edited this in the GitHub");
 
 }
