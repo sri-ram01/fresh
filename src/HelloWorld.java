0aab329f34ac8b871c2ffa95126b40252e8529d6
sri-ram01 <sri_ram01@yahoo.com>
2017-01-22 16:57:40 +0530

    updated the first print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 public static void public static void main(String[] args) {
-	System.out.println("Hello World - my first program");
+	System.out.println("Hello World -ok baba program");
 	System.out.println("I edited this in the GitHub");
 
 }
